265c606358ab2c8a29defba3d34d0ebb2512719f
ushanemala <ushanemala05@gmail.com>
2026-03-05 22:54:40 +0530

    Fix render port

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ def result():
             suggestions=suggestions
         )
     return "Invalid file type. Please upload PDF or Image."
+import os
+
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
